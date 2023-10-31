45dbc8a4eb62cb21a316e1a49c97d7a009274604
Riza Semih Koca <rsemihkoca@outlook.com>
2023-11-01 00:50:13 +0300

    commit

--- client.py
+++ client.py
@@ -5,15 +5,22 @@ import argparse
 
 def main():
 
+    
+    pass
 
-    parser = argparse.ArgumentParser(description='Transmit CSV to REST API and generate Excel')
-    parser.add_argument('-k', '--keys', nargs='+', help='Additional columns to include in the Excel file')
-    parser.add_argument('-c', '--colored', action='store_true', help='Color rows based on hu age')
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Client script for sending CSV data to the server.")
+    parser.add_argument("-f", "--csv-file", required=True, help="Input CSV file")
+    parser.add_argument("-k", "--keys", nargs='+', help="Additional columns to include in the Excel file")
+    parser.add_argument("-c", "--colored", action="store_true", help="Color rows based on hu age")
     args = parser.parse_args()
 
     print("Parsed arguments:")
     print(f"Keys: {args.keys}")
     print(f"Colored: {args.colored}")
 
-if __name__ == '__main__':
     main()
